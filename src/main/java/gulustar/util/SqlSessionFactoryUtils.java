e14fa6df1b303b4799ac1f35febc950c02870b6c
night520 <1009862149@qq.com>
2022-08-05 01:36:51 +0900

    gulustar-init

---
+++ src/main/java/gulustar/util/SqlSessionFactoryUtils.java
@@ -0,0 +1,33 @@
+package gulustar.util;
+
+import org.apache.ibatis.io.Resources;
+import org.apache.ibatis.session.SqlSessionFactory;
+import org.apache.ibatis.session.SqlSessionFactoryBuilder;
+
+import java.io.IOException;
+import java.io.InputStream;
+
+/**
+ * 获取SqlSessionFactory实例
+ */
+public class SqlSessionFactoryUtils {
+    private static SqlSessionFactory sqlSessionFactory;
+
+    static {
+        try {
+            String resource = "mybatis-config.xml";
+            InputStream inputStream = Resources.getResourceAsStream(resource);
+            sqlSessionFactory = new SqlSessionFactoryBuilder().build(inputStream);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    /**
+     * get sqlSessionFactory instance
+     * @return
+     */
+    public static SqlSessionFactory getSqlSessionFactory(){
+        return sqlSessionFactory;
+    }
+}
