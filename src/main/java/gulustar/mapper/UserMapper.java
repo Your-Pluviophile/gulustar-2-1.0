32af57e6794570f49cb99d83c7d9da856a0fa590
fddxi <1531645372@qq.com>
2022-08-10 22:37:51 +0800

    UserMapper初步编写.

--- src/main/java/gulustar/mapper/UserMapper.java
+++ src/main/java/gulustar/mapper/UserMapper.java
@@ -1,15 +1,57 @@
 package gulustar.mapper;
 
 import gulustar.pojo.User;
-import org.apache.ibatis.annotations.Select;
+import org.apache.ibatis.annotations.*;
 
 public interface UserMapper {
 
     // 8.10 elia
-//    @Select()
-    User login();
+
+    /**
+     * 查询用户是否存在
+     * @param account
+     * @param password
+     * @return
+     */
+    @Select("select * from user where account = #{account}")
+    @ResultMap("userResultMap")
+    User selectByAccount(@Param("account") String account, @Param("password") String password);
+
+
+    /**
+     * 尝试登录
+     * @param account
+     * @param password
+     * @return
+     */
+    @Select("select * from user where account = #{account} and password = #{password}")
+    @ResultMap("userResultMap")
+    User login(@Param("account") String account, @Param("password") String password);
+
+
 
 
     //8.10 悠悠球
-    boolean registe();
+
+    /**
+     * 注册  将用户已填写的数据封装成user对象,再调用此方法
+     * @param user
+     * @return
+     */
+    @Insert("insert into user values " +
+            "(#{null},#{username}, #{account},#{password},#{is_admin},#{status})")
+    void register(User user);
+
+    /**
+     * 更新用户数据
+     * 将更改后的用户数据封装成user对象,再调用此方法
+     * @param user
+     */
+    @Update("UPDATE user SET " +
+            "username = #{username},password = #{password}," +
+            "is_admin = #{is_admin},status = #{status} " +
+            "where account=#{account}")
+    void updateByAccount(User user);
+
+
 }
--- user-blog.sql
+++ user-blog.sql
@@ -1,12 +1,12 @@
-CREATE DATABASE gulustar_DB;
-USE DATABASE gulustar_DB;
+CREATE DATABASE if not exists gulustar_DB;
+USE gulustar_DB;
 
 
 #------------------以下为用户相关-------------------------------------
 
 # 用户表
 CREATE TABLE USER(
-	id INT PRIMARY AUTO_INCREMENT,
+	id INT PRIMARY key AUTO_INCREMENT,
 	username VARCHAR(10),
 	`account` VARCHAR(12),
 	`password` VARCHAR(20),
@@ -24,13 +24,13 @@ CREATE TABLE user_collection(
 CREATE TABLE user_follow(
 	user_id INT,
 	follow_id INT #关注的人的ID
-)
+);
 
 #历史浏览记录
 CREATE TABLE user_history(
 	user_id INT,
 	blog_id INT
-)
+);
 
 #------------------以上为用户相关-------------------------------------
 
@@ -40,7 +40,7 @@ CREATE TABLE user_history(
 
 # 博客表
 CREATE TABLE blog(
-	id INT PRIMARY AUTO_INCREMENT,
+	id INT PRIMARY key AUTO_INCREMENT,
 	user_id INT,				#作者
 	category INT,
 	title VARCHAR(100),
@@ -51,23 +51,23 @@ CREATE TABLE blog(
 	STATUS INT,
 	release_date TIMESTAMP,
 	modify_date TIMESTAMP
-)
+);
 
 # 分类表
 CREATE TABLE category(
-	id INT PRIMARY,
+	id INT PRIMARY key,
 	`name` VARCHAR(20)
-)
+);
 
 # 博客的评论
 CREATE TABLE blog_comment(
 	blog_id INT,
 	comment_id INT
-)
+);
 
 # 评论表
 CREATE TABLE `comment`(
-	id INT PRIMARY AUTO_INCREMENT,
+	id INT PRIMARY key AUTO_INCREMENT,
 	content VARCHAR(1000),
 	user_id INT,
 	createTime TIMESTAMP
