06f04ad23409399bb45ce67e960a6436ecd4cb7d
night520 <1009862149@qq.com>
2022-08-11 23:57:30 +0900

    first day

--- src/main/java/gulustar/servlets/UserServlet.java
+++ src/main/java/gulustar/servlets/UserServlet.java
@@ -1,6 +1,7 @@
 package gulustar.servlets;
 
 import com.alibaba.fastjson.JSON;
+import gulustar.pojo.LoginInfos;
 import gulustar.pojo.User;
 import gulustar.service.UserService;
 import gulustar.service.impl.UserServiceImpl;
@@ -27,22 +28,22 @@ public class UserServlet extends BaseServlet {
         //获取账号和密码
         BufferedReader reader = req.getReader();
         String params = reader.readLine();
-        String[] loginInfos = JSON.parseObject(params, String[].class);
-        String account = loginInfos[0];
-        String password = loginInfos[1];
+        LoginInfos loginInfos = JSON.parseObject(params, LoginInfos.class);
+        String account = loginInfos.getAccount();
+        String password = loginInfos.getPassword();
 
         //登录业务处理
         User user = userService.login(account, password);
 
-        //判断用户是否存在
-        if (user != null) {
-            //存在说明登录成功
-            resp.getWriter().write("登录成功！");
-            //将用户存到session中
+        //将结果返回 如果用户不存在会返回null
+        String jsonString = JSON.toJSONString(user);
+        resp.setContentType("text/json;charset=utf-8");
+        resp.getWriter().write(jsonString);
+
+        //如果用户存在,将用户存到session中,避免重复登录
+        if (user != null){
             HttpSession session = req.getSession();
             session.setAttribute("user", user);
-        } else {
-            resp.getWriter().write("登录失败,请检查用户名密码！");
         }
     }
 
