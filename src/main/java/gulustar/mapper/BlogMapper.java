7b23421c7ff8470c3bcd68d3ddf6315685a6428d
wdj <1583503255@qq.com>
2022-08-12 17:29:19 +0800

    8.11dejin

--- src/main/java/gulustar/mapper/BlogMapper.java
+++ src/main/java/gulustar/mapper/BlogMapper.java
@@ -1,8 +1,8 @@
 package gulustar.mapper;
 
 import gulustar.pojo.Blog;
-import org.apache.ibatis.annotations.ResultMap;
-import org.apache.ibatis.annotations.Select;
+import gulustar.pojo.User;
+import org.apache.ibatis.annotations.*;
 
 public interface BlogMapper {
 
@@ -16,4 +16,27 @@ public interface BlogMapper {
     @Select("select * from blog")
     @ResultMap("blogResultMap")
     Blog[] selectAll();
+/*
+更新博客
+ */
+    @Update("UPDATE blog SET " +
+            "user_id = #{user_id},category= #{category}," +
+            "title = #{title},description= #{description} ," +
+            "content= #{content},likes= #{likes}," +
+            "release_date= #{releaseDate},modify_date= now()," +
+            "collected = #{colleted},STATUS = #{status} " +
+            "where id=#{id}")
+    void updateBlog(Blog blog);
+/*
+添加博客
+ */
+    @Insert("insert into blog values " +
+            "(null, #{userId}, #{category}, #{title}, #{description}, #{content}, #{likes}," +
+            " #{collected}, #{status}, now(), now())")
+    boolean addBlog(Blog blog);
+    /*
+    删除博客
+     */
+    @Delete("delete from blog where id=#{id}")
+    boolean deleteBlog(Blog blog);
 }
